97efc984110c827e597a5c50951cbd264bbfba73
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2024-07-13 17:19:33 -0500

    Routing Done

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hola mi server en express');
+  res.send('Hola mi server es Express');
 })
+
+app.get('/questions',(req, res) => {
+  console.log('Mis Preguntas')
+  res.json({
+    title: 'Health Detector',
+    id: 1,
+  });
+});
 
 app.listen(port, () => {
   console.log('Mi port' +  port);
