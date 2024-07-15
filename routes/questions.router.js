cbdcb1337dabc614fbe1107882b04fcdfae0be0f
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2024-07-15 11:26:58 -0500

    Implemented

--- routes/questions.router.js
+++ routes/questions.router.js
@@ -9,17 +9,23 @@ const questions_library = [
 {id: 5, title: 'Mental Health', text:"Managing stress, and improving mental well-being", completed: false},
 {id: 6, title: 'Sleep Well', text:"Improve sleep habits", completed: false}]
 
+const question_arr = [
+  { title: 'Health and Wellness', text:"Physical and Mental Well-being", completed: true},
+  {title: 'Popular Health', text:"Weight Loss", completed: false}
+]
+
 router.get('/', (req, res) => {
   res.json(questions_library);
 });
 
 router.get('/:id',(req, res) => {
   const { id } = req.params;
-  const question = questions_library.find(question => question.id === parseInt(id));
-  if(!question){
+  const questionIndex = questions_library.findIndex(question => question.id === parseInt(id));
+  if(!questionIndex && questionIndex === -1 ){
    return res.status(404).json({ message: 'Question not found'});
   }
-  res.json(question);
+  const deletedQuestion = questions_library.splice(questionIndex, 1)
+  res.json({ message: 'Pregunta exitosamente eliminada', deletedQuestion});
 });
 
 module.exports = router;
