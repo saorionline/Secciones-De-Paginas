95a41f4c3a9a2a780edea43793525318fbf726ba
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2024-07-17 20:36:00 -0500

    React Components

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const routerApi = require('./routes');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const Hero = require('./react-components/Hero.tsx'); // Replace with your component path
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Home');
+  const reactComponent = ReactDOMServer.renderToString(< Hero/>);
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>My Express App by Sao</title>
+    </head>
+    <body>
+      <div id="root">${reactComponent}</div>
+      <script ></script> // Replace with actual path src="path/to/your/client-side-react-bundle.js"
+    </body>
+    </html>
+  `);
   });
+
 app.get('/new-route', (req, res) => {
   res.send('Page 1');
  }); 
