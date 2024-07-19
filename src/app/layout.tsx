0da947c94d8a300e93a99191f925013bb3a05076
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2024-07-19 15:18:22 -0500

    New Health Content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import MobileMenu from '../components/MobileMenu.js'; // Assuming 'components' directory
+import Features from '../components/Features.js';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <MobileMenu />
+        <Features />
         {children}
       </body>
     </html>
