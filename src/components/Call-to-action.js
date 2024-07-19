fd526d8bd9f0ebe08276706b0164c00abdc90572
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2024-07-19 15:45:21 -0500

    Lecturas

--- src/components/Call-to-action.js
+++ src/components/Call-to-action.js
@@ -18,22 +18,22 @@ export default function Example() {
           </svg>
           <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
             <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-              Monitorea tu deficiencia del hierro.
+              Monitorea tu Ferritina.
               <br />
-              Un análisis de sangre bastará para saber los niveles
+              Un análisis de sangre bastará.
             </h2>
             <p className="mt-6 text-lg leading-8 text-gray-300">
-              La principal proteína de almacenamiento de hierro en el cuerpo se encuentra en la Ferritina. Ideal para diagnosticas ciertos estados de salud del cuerpo. 
+              La principal proteína de almacenamiento de hierro en el cuerpo se encuentra en la Ferritina. Saber cuáles son sus nivele es ideal para diagnosticar ciertos estados de salud del cuerpo. 
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
               <a
                 href="#"
                 className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
               >
-                Get started
+                Comienza ya
               </a>
               <a href="#" className="text-sm font-semibold leading-6 text-white">
-                Learn more <span aria-hidden="true">→</span>
+                Leer más <span aria-hidden="true">→</span>
               </a>
             </div>
           </div>
