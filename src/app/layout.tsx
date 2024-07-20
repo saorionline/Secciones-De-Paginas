8fd5501237d9d955ea9e8e4948a14d022020d5aa
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2024-07-19 19:04:44 -0500

    Tailwind Componentes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import MobileMenu from '../components/MobileMenu.js'; // Assuming 'components' directory
-import Features from '../components/Features.js';
-import CallToAction from '../components/Call-to-action.js';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,8 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <MobileMenu />
-        <Features />
-        <CallToAction />
+        
         {children}
       </body>
     </html>
